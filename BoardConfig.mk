6aff7d8214f140e52b1188c1a25e9dd19ca9f011
Artem Borisov <dedsa2002@gmail.com>
2017-12-30 15:12:35 +0000

    ginkgo: Enable boot and system server dex-preopt
    As stated in https://android.googlesource.com/platform/build/+/418258c
    this is required in O-MR1 since sepolicy now prevents system server from
    loading anything from /data making our system extremely slow and janky.
    
    Change-Id: Ifc820db58eda093726eb120dd83fb4e66d2ad8f8
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -94,6 +94,14 @@ USE_DEVICE_SPECIFIC_CAMERA := true
 # Charger Mode
 BOARD_CHARGER_ENABLE_SUSPEND := true
 
+# Dexpreopt
+ifeq ($(HOST_OS),linux)
+ifneq ($(TARGET_BUILD_VARIANT),eng)
+WITH_DEXPREOPT := true
+WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
+endif
+endif
+
 # DRM
 TARGET_ENABLE_MEDIADRM_64 := true
 
