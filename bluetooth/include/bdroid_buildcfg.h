e930419af527762cb9f6dd59af3e66dce5e1ff6c
Prateek Chaubey <chaubeyprateek@gmail.com>
2018-01-29 15:44:29 +0000

    ginkgo: bluetooth: Unset BTM_DEF_LOCAL_NAME
    * Automatically set bluetooth name based on ro.product.model
    
    Change-Id: Ie5659765b27d923c2d27405d694656d1361c4853
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -23,13 +23,15 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME "Redmi Note 8"
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
 #define MAX_L2CAP_CHANNELS    16
 #define BLE_VND_INCLUDED   TRUE
+
 // Skips conn update at conn completion
 #define BT_CLEAN_TURN_ON_DISABLED 1
+
 // Increasing SEPs to 12 from 6 to support SHO/MCast i.e. two streams per codec
 #define AVDT_NUM_SEPS 12
+
 #endif
