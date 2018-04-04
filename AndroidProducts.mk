a14ed797fe0745dfc637ca2f81658ab886f1cbf3
Dan Willemsen <dwillemsen@google.com>
2018-04-04 16:49:07 -0700

    ginkgo: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
    Bug: 77599627
    Test: lunch  [see entries in list]
    Change-Id: Id44fcfec47eeb979a745f879031f1bec7359a633

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,12 @@
 #
-# Copyright (C) 2019 The LineageOS Project
+# Copyright (C) 2017-2020 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_ginkgo.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_ginkgo-eng \
+    lineage_ginkgo-userdebug
