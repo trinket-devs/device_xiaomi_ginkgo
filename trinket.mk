74afaa34f0c9fa516ecf7e97644d88104a16bb6a
Demon Singur <demonsingur@gmail.com>
2018-06-23 05:22:18 +0000

    trinket-common: Build separate system and vendor HIDL libs
    Change-Id: If8e2211e643bdb0ff67fb248e7fe33b78077a06e
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- trinket.mk
+++ trinket.mk
@@ -144,7 +144,9 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.manager@1.0
+    android.hidl.base@1.0_system \
+    android.hidl.manager@1.0 \
+    android.hidl.manager@1.0_system
 
 # Hotword Enrollment
 PRODUCT_COPY_FILES += \
