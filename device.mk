4de264919ba0ff05c5e3e20fbb6fe890d9c6e7d8
1nfrag <sil3ntcll@gmail.com>
2018-10-30 21:18:02 +0800

    ginkgo: Build NoCutoutOverlay
    Change-Id: I94d80335f58217d0af91273ad1ae476f69ba07a9

--- device.mk
+++ device.mk
@@ -28,6 +28,9 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage/lineage-sdk
 
+PRODUCT_PACKAGES += \
+    NoCutoutOverlay
+
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
