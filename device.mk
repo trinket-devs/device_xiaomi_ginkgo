43c57a0bc3b76e28fa55362f916f7d462ce2ce19
Offain <fabian.leutenegger@bluewin.ch>
2019-02-06 17:40:52 +0000

    ginkgo: Adapt ANT+ for CAF

--- device.mk
+++ device.mk
@@ -139,10 +139,8 @@ PRODUCT_COPY_FILES += \
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService \
-    com.dsi.ant.antradio_library
-
-PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
+    antradio_app \
+    libantradio
 
 # Atrace
 PRODUCT_PACKAGES += \
