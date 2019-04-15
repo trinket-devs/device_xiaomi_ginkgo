2294e937179d8841bf171fa51eeb07c6cf4abd36
Jiyong Park <jiyong@google.com>
2019-04-15 14:19:03 +0900

    ginkgo: Add updatable_apex.mk
    The product abstracts configs that need to be set to support updating of APEXes.
    
    Change-Id: Id5aa944ceba6a6846308f1c18a08af8b3bb38589

--- lineage_ginkgo.mk
+++ lineage_ginkgo.mk
@@ -9,6 +9,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Inherit from ginkgo device
 $(call inherit-product, device/xiaomi/ginkgo/device.mk)
 
