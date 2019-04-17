2b8ef3c851d1c20b7461764f95b0b86b7984fe87
GuaiYiHu <guaiyihu@foxmail.com>
2019-04-17 16:57:34 +0800

    ginkgo: Build broadcastradio hidl impl
    Change-Id: I206755bf3f3a34337c48608156043d6849c67008

--- device.mk
+++ device.mk
@@ -142,6 +142,9 @@ PRODUCT_PACKAGES += \
     libqcomfm_jni \
     qcom.fmradio
 
+PRODUCT_PACKAGES += \
+    android.hardware.broadcastradio@1.0-impl
+
 # Freeform Multiwindow
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
