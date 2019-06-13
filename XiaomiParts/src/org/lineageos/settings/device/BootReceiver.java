adb45a706a8f451b69796c034a233fd30c462c09
Stylogey <stylogey@gmail.com>
2019-06-13 21:49:01 +0200

    ginkgo: XiaomiParts: Add USB fastcharge switch
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- XiaomiParts/src/org/lineageos/settings/device/BootReceiver.java
+++ XiaomiParts/src/org/lineageos/settings/device/BootReceiver.java
@@ -57,6 +57,8 @@ public class BootReceiver extends BroadcastReceiver implements Utils {
 
         FileUtils.setValue(DeviceSettings.VIBRATION_STRENGTH_PATH, Settings.Secure.getInt(
                 context.getContentResolver(), DeviceSettings.PREF_VIBRATION_STRENGTH, 80) / 100.0 * (DeviceSettings.MAX_VIBRATION - DeviceSettings.MIN_VIBRATION) + DeviceSettings.MIN_VIBRATION);
+        FileUtils.setValue(DeviceSettings.USB_FASTCHARGE_PATH, Settings.Secure.getInt(context.getContentResolver(),
+                DeviceSettings.PREF_USB_FASTCHARGE, 0));
         context.startService(new Intent(context, DiracService.class));
     }
 }
