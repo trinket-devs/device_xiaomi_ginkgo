898f6f06cf2e5da2c5394fc032520b9dde45b270
LuK1337 <priv.luk@gmail.com>
2019-07-02 23:25:02 +0200

    ginkgo: Get rid of framework_*.xml
     * Unused ^-^.
    
    Change-Id: I89db15b83564fc7fc0081afbc1b53b7a1caa46aa
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -127,7 +127,6 @@ TARGET_USES_HWC2 := true
 TARGET_USES_ION := true
 
 # HIDL
-DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 
