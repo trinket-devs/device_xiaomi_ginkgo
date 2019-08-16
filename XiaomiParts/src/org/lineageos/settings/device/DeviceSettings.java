39afa8e0a13cb089c87ff014b43959e634682962
Stylogey <stylogey@gmail.com>
2019-08-16 08:35:51 +0200

    [Squashed] ginkgo: Import XiaomiParts
    From bfcc56f8f80fb4314389dccb40f46998de8d6bb5 Mon Sep 17 00:00:00 2001
    From: Bruno Martins <bgcngm@gmail.com>
    Date: Mon, 16 Sep 2019 17:25:48 +0200
    Subject: [PATCH] sdm660-common: XiaomiParts: Migrate to androidx
    
     * Cleanup some unneded files
    
    From 08784fd3140ef3621522086d9d7e136a38ca93da Mon Sep 17 00:00:00 2001
    From: darkbeast69 <guptaaryan189@gmail.com>
    Date: Tue, 15 Oct 2019 10:58:29 +0000
    Subject: [PATCH] sdm660: Add Vector Icons to XiaomiParts
    
     * It looks dope icons from https://materialdesignicons.com
    
    From 702f8d64b9105f7b68babb1551aee43b6e5960d3 Mon Sep 17 00:00:00 2001
    From: Aryan <guptaaryan189@gmail.com>
    Date: Sat, 19 Oct 2019 07:45:46 +0000
    Subject: [PATCH] sdm660:Make vector images use the same accent color as the rest of the system!
    
    From d9cde63c35370fb9541a277a34f062b1170c0071 Mon Sep 17 00:00:00 2001
    From: Stylogey <stylogey@gmail.com>
    Date: Sat, 19 Oct 2019 17:20:42 +0200
    Subject: [PATCH] sdm660-common: XiaomiParts: Change settings icon to MI logo
    
    From 197013de2eacd66674e38dcaddbb22ea5b4bace1 Mon Sep 17 00:00:00 2001
    From: Stylogey <stylogey@gmail.com>
    Date: Sat, 19 Oct 2019 19:04:14 +0200
    Subject: [PATCH] sdm660-common: XiaomiParts: Add a preview image to kcal
    
    From 96cf920455d1c59e5acf9c55f58c36bead734905 Mon Sep 17 00:00:00 2001
    From: Stylogey <stylogey@gmail.com>
    Date: Sat, 19 Oct 2019 19:10:00 +0200
    Subject: [PATCH] sdm660-common: XiaomiParts: Dont crash if dirac is not initialized
    
    From 356f5131029256b5d16bb402222ff9e37bd8ab7c Mon Sep 17 00:00:00 2001
    From: Dušan Uverić <dusan.uveric9@gmail.com>
    Date: Wed, 15 Apr 2020 21:14:49 +0200
    Subject: [PATCH] sdm660-common: XiaomiParts: check if vibrator strength is supported before adding
    
    Co-authored-by: Bruno Martins <bgcngm@gmail.com>
    Co-authored-by: darkbeast69 <guptaaryan189@gmail.com>
    Co-authored-by: Aryan <guptaaryan189@gmail.com>
    Co-authored-by: Dušan Uverić <dusan.uveric9@gmail.com>
    Co-authored-by: Mrinal Ghosh <mg712702@gmail.com>
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

---
+++ XiaomiParts/src/org/lineageos/settings/device/DeviceSettings.java
@@ -0,0 +1,154 @@
+/*
+ * Copyright (C) 2018-2019 The Xiaomi-SDM660 Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+package org.lineageos.settings.device;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+
+import org.lineageos.settings.device.kcal.KCalSettingsActivity;
+import org.lineageos.settings.device.preferences.SecureSettingListPreference;
+import org.lineageos.settings.device.preferences.SecureSettingSwitchPreference;
+import org.lineageos.settings.device.preferences.VibrationSeekBarPreference;
+import org.lineageos.settings.device.preferences.CustomSeekBarPreference;
+
+import java.lang.Math.*;
+
+public class DeviceSettings extends PreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String PREF_ENABLE_DIRAC = "dirac_enabled";
+    private static final String PREF_HEADSET = "dirac_headset_pref";
+    private static final String PREF_PRESET = "dirac_preset_pref";
+
+    private static final String CATEGORY_DISPLAY = "display";
+    private static final String PREF_DEVICE_DOZE = "device_doze";
+    private static final String DEVICE_DOZE_PACKAGE_NAME = "org.lineageos.settings.doze";
+
+    private static final String PREF_DEVICE_KCAL = "device_kcal";
+
+    public static final String CATEGORY_VIBRATOR = "vibration";
+    public static final String PREF_VIBRATION_STRENGTH = "vibration_strength";
+    public static final String VIBRATION_STRENGTH_PATH = "/sys/devices/virtual/timed_output/vibrator/vtg_level";
+
+    // value of vtg_min and vtg_max
+    public static final int MIN_VIBRATION = 116;
+    public static final int MAX_VIBRATION = 3596;
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.preferences_xiaomi_parts, rootKey);
+
+        boolean enhancerEnabled;
+        try {
+            enhancerEnabled = DiracService.sDiracUtils.isDiracEnabled();
+        } catch (java.lang.NullPointerException e) {
+            getContext().startService(new Intent(getContext(), DiracService.class));
+            try {
+                enhancerEnabled = DiracService.sDiracUtils.isDiracEnabled();
+            } catch (NullPointerException ne) {
+                // Avoid crash
+                ne.printStackTrace();
+                enhancerEnabled = false;
+            }
+        }
+
+        SecureSettingSwitchPreference enableDirac = (SecureSettingSwitchPreference) findPreference(PREF_ENABLE_DIRAC);
+        enableDirac.setOnPreferenceChangeListener(this);
+        enableDirac.setChecked(enhancerEnabled);
+
+        SecureSettingListPreference headsetType = (SecureSettingListPreference) findPreference(PREF_HEADSET);
+        headsetType.setOnPreferenceChangeListener(this);
+
+        SecureSettingListPreference preset = (SecureSettingListPreference) findPreference(PREF_PRESET);
+        preset.setOnPreferenceChangeListener(this);
+
+        PreferenceCategory displayCategory = (PreferenceCategory) findPreference(CATEGORY_DISPLAY);
+        if (isAppNotInstalled(DEVICE_DOZE_PACKAGE_NAME)) {
+            displayCategory.removePreference(findPreference(PREF_DEVICE_DOZE));
+        }
+
+        Preference kcal = findPreference(PREF_DEVICE_KCAL);
+
+        kcal.setOnPreferenceClickListener(preference -> {
+            Intent intent = new Intent(getActivity().getApplicationContext(), KCalSettingsActivity.class);
+            startActivity(intent);
+            return true;
+        });
+
+        if (FileUtils.fileWritable(VIBRATION_STRENGTH_PATH)) {
+            VibrationSeekBarPreference vibrationStrength = (VibrationSeekBarPreference) findPreference(PREF_VIBRATION_STRENGTH);
+            vibrationStrength.setOnPreferenceChangeListener(this);
+        } else { getPreferenceScreen().removePreference(findPreference(CATEGORY_VIBRATOR)); }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object value) {
+        final String key = preference.getKey();
+        switch (key) {
+
+            case PREF_ENABLE_DIRAC:
+                try {
+                    DiracService.sDiracUtils.setEnabled((boolean) value);
+                } catch (java.lang.NullPointerException e) {
+                    getContext().startService(new Intent(getContext(), DiracService.class));
+                    DiracService.sDiracUtils.setEnabled((boolean) value);
+                }
+                break;
+
+            case PREF_HEADSET:
+                try {
+                    DiracService.sDiracUtils.setHeadsetType(Integer.parseInt(value.toString()));
+                } catch (java.lang.NullPointerException e) {
+                    getContext().startService(new Intent(getContext(), DiracService.class));
+                    DiracService.sDiracUtils.setHeadsetType(Integer.parseInt(value.toString()));
+                }
+                break;
+
+            case PREF_PRESET:
+                try {
+                    DiracService.sDiracUtils.setLevel(String.valueOf(value));
+                } catch (java.lang.NullPointerException e) {
+                    getContext().startService(new Intent(getContext(), DiracService.class));
+                    DiracService.sDiracUtils.setLevel(String.valueOf(value));
+                }
+                break;
+
+            case PREF_VIBRATION_STRENGTH:
+                double vibrationValue = (int) value / 100.0 * (MAX_VIBRATION - MIN_VIBRATION) + MIN_VIBRATION;
+                FileUtils.setValue(VIBRATION_STRENGTH_PATH, vibrationValue);
+                break;
+
+            default:
+                break;
+        }
+        return true;
+    }
+
+    private boolean isAppNotInstalled(String uri) {
+        PackageManager packageManager = getContext().getPackageManager();
+        try {
+            packageManager.getPackageInfo(uri, PackageManager.GET_ACTIVITIES);
+            return false;
+        } catch (PackageManager.NameNotFoundException e) {
+            return true;
+        }
+    }
+}
--- device.mk
+++ device.mk
@@ -319,10 +319,6 @@ PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
 
-# Parts
-PRODUCT_PACKAGES += \
-    XiaomiParts
-
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
@@ -458,3 +454,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_BOOT_JARS += \
     WfdCommon
+
+# XiaomiParts
+PRODUCT_PACKAGES += \
+    XiaomiParts
