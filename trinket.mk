3503893f9aae1c7d951fc01242cb59e42e5b6e6a
Mikhail Naganov <mnaganov@google.com>
2019-11-22 17:34:45 -0800

    trinket-common: Uprev audio HAL to v6
    Test: VtsHalAudioV6_0TargetTest VtsHalAudioEffectV6_0TargetTest
    Bug: 155591007
    Change-Id: If40ae28c224160728634736ee95590a779f905c4
    (cherry picked from commit f11963392c9afd79704822a5812ef4412f3810db)

--- trinket.mk
+++ trinket.mk
@@ -18,9 +18,9 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@5.0-impl \
+    android.hardware.audio@6.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@5.0-impl \
+    android.hardware.audio.effect@6.0-impl \
     android.hardware.soundtrigger@2.1-impl \
     audio.a2dp.default \
     audio.r_submix.default \
