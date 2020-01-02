8640e6d154a6995d1e26e5a1dd8b405951895023
Dyneteve <32036579+Dyneteve@users.noreply.github.com>
2020-01-02 18:03:55 +0200

    ginkgo: Add missing display props
    * Disable inline rotor
    * Disable excl partial
    * Disable null display

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -55,8 +55,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.display.comp_mask=0 \
     vendor.display.dataspace_saturation_matrix=1.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,1.0 \
     vendor.display.disable_decimation=1 \
+    vendor.display.disable_excl_rect=0 \
+    vendor.display.disable_excl_rect_partial_fb=1 \
     vendor.display.disable_hw_recovery_dump=1 \
+    vendor.display.disable_inline_rotator=1 \
+    vendor.display.disable_scaler=0 \
     vendor.display.enable_default_color_mode=0 \
+    vendor.display.enable_null_display=0 \
     vendor.gralloc.disable_ubwc=0
 
 # Dalvik
