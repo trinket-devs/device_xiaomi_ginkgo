5a61d4ebb11a7cbd5c58f857e34b954557453a00
Dyneteve <dyneteve@pixelexperience.org>
2020-01-04 18:35:55 +0200

    ginkgo: Drop prebuilt libdng_sdk

--- device.mk
+++ device.mk
@@ -136,7 +136,8 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    vendor.qti.hardware.camera.device@1.0.vendor
+    vendor.qti.hardware.camera.device@1.0.vendor \
+    libdng_sdk.vendor
 
 # Context Hub
 PRODUCT_PACKAGES += \
