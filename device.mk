bb3ad58acc354c81ddd0285e8be158d7f8a01c3c
ManoloRey <cleitonmca@gmail.com>
2020-01-09 00:03:39 +0000

    ginkgo: Enable Camera Snap

--- device.mk
+++ device.mk
@@ -137,7 +137,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0.vendor \
-    libdng_sdk.vendor
+    libdng_sdk.vendor \
+    Snap
 
 # Context Hub
 PRODUCT_PACKAGES += \
