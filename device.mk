d9dc5cda1f23d4d9f233d8e182ad21cd79aa7c34
ManoloRey <cleitonmca@gmail.com>
2020-01-11 00:24:06 +0000

    ginkgo: Fix QuickSwitch support

--- device.mk
+++ device.mk
@@ -21,7 +21,8 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-pe
 
-PRODUCT_ENFORCE_RRO_TARGETS := *
+PRODUCT_ENFORCE_RRO_TARGETS := \
+    framework-res
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
@@ -269,7 +270,7 @@ PRODUCT_COPY_FILES += \
 # IRSC
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
-    
+
 # Keylayouts
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/trinket-idp-snd-card_Button_Jack.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/trinket-idp-snd-card_Button_Jack.kl \
