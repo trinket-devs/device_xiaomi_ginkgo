2cac33cb93fb2763dd6af1937473016b3a11d63a
officialyash007 <officialyash007@gmail.com>
2020-01-13 04:14:15 +0000

    ginkgo: Exclude lineage-sdk overlays from RRO * lineage-sdk overlays don't work with RRO * needed for force full screen apps for notch device for crdroid * thanks @invisiblek
    Signed-off-by: officialyash007 <officialyash007@gmail.com>
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- device.mk
+++ device.mk
@@ -28,6 +28,8 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
