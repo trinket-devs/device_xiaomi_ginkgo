a340e5d429d000e943d025e4ba89a1477a94772f
FatalPy <jimmy.nelle@live.de>
2020-01-17 19:05:57 +0100

    NO GAPPS

--- derp_ginkgo.mk
+++ derp_ginkgo.mk
@@ -13,11 +13,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Inherit from ginkgo device
 $(call inherit-product, device/xiaomi/ginkgo/device.mk)
 
-# Inherit some common PixelExperience stuff.
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_WIFI_EXT := true
 TARGET_INCLUDE_STOCK_ARCORE := true
+DERP_BUILDTYPE=Official
+CURRENT_BUILD_TYPE=nogapps
 $(call inherit-product, vendor/aosip/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
@@ -27,7 +27,6 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 8
 PRODUCT_MANUFACTURER := Xiaomi
 
-export DERP_BUILDTYPE=Official
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="ginkgo" \
