44f3e49f651ebef6b285631d34ecdd9f9a205822
FatalPy <jimmy.nelle@live.de>
2020-02-02 14:18:50 +0100

    ginkgo: removed updateable_apex

--- derp_ginkgo.mk
+++ derp_ginkgo.mk
@@ -8,7 +8,6 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Inherit from ginkgo device
 $(call inherit-product, device/xiaomi/ginkgo/device.mk)
