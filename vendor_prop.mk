7521de9544704eb03b20e9fed584973969a7ea7a
FatalPy <jimmy.nelle@live.de>
2020-02-09 22:00:50 +0100

    ginkgo: add prop

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -72,6 +72,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.enable_hwc_vds=1 \
     persist.hwc.enable_vds=1 \
     debug.sf.hw=0 \
+    debug.egl.hw=0 \
     debug.sf.recomputecrop=0 \
     debug.sf.latch_unsignaled=0 \
     debug.sf.disable_backpressure=1 \
