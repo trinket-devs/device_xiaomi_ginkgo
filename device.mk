f6d292942f860a767ee849a6b5e10f3e01640bf1
Tapas Dey <tdey@codeaurora.org>
2020-02-13 18:53:21 +0530

    ginkgo: NFC: Remove NQ jar as part of product boot
    Previously when 3rd party modules were built under system partition,
    NQ jar file was being read from /system/frameworks path.
    As part of Android upgrade requirement, vendor or
    3rd party module should now be built under system ext partition.
    
    After moving to system ext partition, if we use NQ jar as product boot
    it's causing device boot up issue. It was added as part of boot because of
    other module dependency which is not valid anymore and hence removed.
    
    Change-Id: I7bebe43a83a44569dcaac99827e94b503caf4c4b

--- device.mk
+++ device.mk
@@ -63,9 +63,6 @@ PRODUCT_PACKAGES += \
     Tag \
     vendor.nxp.hardware.nfc@2.0-service
 
-PRODUCT_BOOT_JARS += \
-    com.nxp.nfc.nq
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
