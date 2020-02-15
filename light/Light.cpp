495de61c7591e0716a44d97bc0aa3b42b98c118f
Demon000 <demonsingur@gmail.com>
2020-02-15 11:14:48 +0000

    ginkgo: light: Do not turn on breathing for unlit hardware states
    Change-Id: I093fb2fb81012a473eda232d3c3407d0e0791907

--- light/Light.cpp
+++ light/Light.cpp
@@ -112,6 +112,11 @@ static void handleNotification(const LightState& state) {
 
     /* Disable breathing */
     set(WHITE_LED BREATH, 0);
+    set(WHITE_LED BRIGHTNESS, 0);
+
+    if (!whiteBrightness) {
+        return;
+    }
 
     switch (state.flashMode) {
         case Flash::HARDWARE:
