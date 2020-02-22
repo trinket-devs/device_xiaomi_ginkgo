aa6ecdbad8ec6f6328ca6160c7a43797b1b1017a
Pranav Vashi <neobuddy89@gmail.com>
2020-02-22 14:20:50 +0000

    ginkgo: Kang walleye offsets for GPU composition
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -126,7 +126,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     telephony.lteOnCdmaDevice=1
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.early_phase_offset_ns=500000 \
-    debug.sf.early_app_phase_offset_ns=500000 \
+    debug.sf.early_phase_offset_ns=1500000 \
+    debug.sf.early_app_phase_offset_ns=1500000 \
     debug.sf.early_gl_phase_offset_ns=3000000 \
     debug.sf.early_gl_app_phase_offset_ns=15000000
