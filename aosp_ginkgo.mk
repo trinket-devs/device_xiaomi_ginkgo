73163fdced399238aafe51e37ed4a2309344693f
FatalPy <jimmy.nelle@live.de>
2020-02-22 21:26:27 +0100

    ginkgo: include MiuiCamera
    * thanks to @DarkDampSquib!
    
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- aosp_ginkgo.mk
+++ aosp_ginkgo.mk
@@ -10,6 +10,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Inherit from custom vendor
+$(call inherit-product, vendor/MiuiCamera/config.mk)
+
 # Inherit from ginkgo device
 $(call inherit-product, device/xiaomi/ginkgo/device.mk)
 
