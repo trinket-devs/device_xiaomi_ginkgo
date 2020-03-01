98457a03c75c2ef25ccdb8b3c78d197bb8413beb
Ethan Chen <intervigil@gmail.com>
2020-03-01 00:34:32 -0800

    ginkgo: Remove unnecessary netutils packages
    * These are already packaged by AOSP.
    
    Change-Id: I77f91fbbb33a6d891d14fe852b094ab914ca3111

--- device.mk
+++ device.mk
@@ -270,10 +270,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
-# Net
-PRODUCT_PACKAGES += \
-    netutils-wrapper-1.0
-
 # NFC
 PRODUCT_SOONG_NAMESPACES += \
     vendor/nxp/opensource/sn100x
