3aab62e81a27d623f835125307651ecacd112cf6
Fenglin Wu <fenglinw@codeaurora.org>
2020-03-05 19:31:14 +0800

    ginkgo: Exclude vibrator devices from input subsystem
    Haptics devices which registers as input FF devices are not supposed to
    be detected by input subsystem. Add excluded-input-devices.xml file for
    input subsystem to exclude them.
    
    Change-Id: I52dc6e502b29e2813299d073195a0991bb2cf692
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- device.mk
+++ device.mk
@@ -411,6 +411,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/input/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
