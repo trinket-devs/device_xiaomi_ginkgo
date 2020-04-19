bd8268f8d01114a7b9febe385156d0434b0dc87b
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-04-20 02:46:31 +0530

    ginkgo: Add powerhint.xml from LA.UM.8.11.r1-03300-NICOBAR.0

--- device.mk
+++ device.mk
@@ -356,6 +356,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+
 # Public Libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
