652417da0f10e24f50323b12f783fb821e56658d
Ayishwarya Narasimhan <ayishwar@codeaurora.org>
2020-04-20 15:27:51 -0700

    ginkgo: config.fs: Add mapping for imsdaemon
    Change-Id: I62629d99e0ace3e5cfa0bdef925e919ddd2ebe12
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- configs/fs/config.fs
+++ configs/fs/config.fs
@@ -46,6 +46,12 @@ user: AID_RADIO
 group: AID_RADIO
 caps: NET_BIND_SERVICE
 
+[vendor/bin/imsdaemon]
+mode: 0755
+user: AID_RADIO
+group: AID_RADIO
+caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
+
 [vendor/bin/imsdatadaemon]
 mode: 0755
 user: AID_RADIO
