539fb94c22c7e18235594bcda0b909751de85041
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-04-21 16:54:40 +0530

    ginkgo: Update wifi/wigig blobs from laurel_sprout

--- device.mk
+++ device.mk
@@ -429,7 +429,7 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf \
     vendor.qti.hardware.wifi.hostapd@1.1.vendor \
-    vendor.qti.hardware.wifi.supplicant@2.1.vendor
+    vendor.qti.hardware.wifi.supplicant@2.0.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/hostapd.accept:$(TARGET_COPY_OUT_VENDOR)/etc/hostapd/hostapd.accept \
