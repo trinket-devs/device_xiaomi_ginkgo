61c2e35985791552c3e744d5ef8c534a0dea6deb
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-04-23 21:22:21 +0530

    ginkgo: Switch to proton clang for building kernel
     * Thanks to kdrag0n for this Clang 11-based toolchain!
    
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -49,6 +49,9 @@ BOARD_RAMDISK_OFFSET := 0x01000000
 BOARD_KERNEL_SEPARATED_DTBO := true
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_VERSION := 11.0
+KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-11.0/bin
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
 TARGET_KERNEL_SOURCE := kernel/xiaomi/ginkgo
 TARGET_KERNEL_CONFIG := vendor/ginkgo-perf_defconfig
 
