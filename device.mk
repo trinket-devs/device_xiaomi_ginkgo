cca986a0dc5638b0c8ab1559102135e0b3c4974a
Mrinal Ghosh <mg712702@gmail.com>
2020-04-25 11:51:53 +0530

    ginkgo: Don't build PixelLiveWallpaperPrebuilt
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- device.mk
+++ device.mk
@@ -458,10 +458,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
 
-# Wallpapers
-PRODUCT_PACKAGES += \
-    PixelLiveWallpaperPrebuilt
-
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
