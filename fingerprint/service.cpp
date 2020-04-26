d1cdb02457c8affdb8427284ea96780a3a76e8c6
cyberwalkman <firefox883@gmail.com>
2020-04-26 22:38:21 +0500

    ginkgo: fingerprint: Fix typo
    Change-Id: I60553a31f3308739c7d39b10ec0ced1c163a5d9d
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- fingerprint/service.cpp
+++ fingerprint/service.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sm6150"
+#define LOG_TAG "android.hardware.biometrics.fingerprint@2.1-service.ginkgo"
 
 #include <android/log.h>
 #include <hidl/HidlTransportSupport.h>
