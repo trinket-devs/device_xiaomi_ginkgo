c069beabcc3cce400d11b446b4637d0fed9c5554
Adithya R <radithya2002@gmail.com>
2020-05-01 20:48:34 +0530

    ginkgo: Switch to prebuilt camera HIDLs
     * From stock MIUI V11.0.8.0.PCOMIXM
     * Probably one of the dirtiest hax you can find these days
     * Blame xiaomeme for fucking with back aux camera ids and numbering
       them 20, 21, 22, 61 etc instead of 1, 2, 3, 4

--- device.mk
+++ device.mk
@@ -150,9 +150,6 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
-    vendor.qti.hardware.camera.device@1.0.vendor \
     libdng_sdk.vendor \
     libshim_camera
 
