11e448dc706b41f91f4e249b5ef0d6049ac65d99
celtare21 <celtare21@gmail.com>
2020-05-05 21:07:25 +0200

    ginkgo: Remove LMKD references
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -53,9 +53,6 @@ TARGET_KERNEL_CLANG_VERSION := proton
 TARGET_KERNEL_SOURCE := kernel/xiaomi/ginkgo
 TARGET_KERNEL_CONFIG := vendor/ginkgo-perf_defconfig
 
-# Enable stats logging in LMKD
-TARGET_LMKD_STATS_LOG := true
-
 # Platform
 TARGET_BOARD_PLATFORM := trinket
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno610
