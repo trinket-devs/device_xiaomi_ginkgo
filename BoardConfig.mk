786f19d4ecdcbef355aae05e4b4fa99ce49a9b6b
celtare21 <celtare21@gmail.com>
2020-05-10 12:14:17 +0200

    ginkgo: this is artemis
    Signed-off-by: billaids <jimmy.nelle@hsw-stud.de>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -51,7 +51,7 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CLANG_VERSION := proton
 TARGET_KERNEL_SOURCE := kernel/xiaomi/ginkgo
-TARGET_KERNEL_CONFIG := vendor/ginkgo-perf_defconfig
+TARGET_KERNEL_CONFIG := artemis_ginkgo_defconfig
 
 # Platform
 TARGET_BOARD_PLATFORM := trinket
