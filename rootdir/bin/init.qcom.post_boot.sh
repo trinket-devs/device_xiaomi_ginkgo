08fd2b16406f3c177200c0ff366ac9eefbfc7a3b
celtare21 <celtare21@gmail.com>
2020-05-13 19:59:04 +0200

    ginkgo: rootdir: Import task cpu affinity script from RenderBroken
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Change-Id: I7919d96f242f4550d1430c460da5bf4ae6932ce8
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -2948,6 +2948,33 @@ case "$target" in
 
             done
 
+            # EXPERIMENTAL: Optimize UX task cgroup membership
+            PIDSS=`ps -AT | grep system_server | awk '{print $3}'`
+            echo $PIDSS > /dev/cpuset/foreground/cgroup.procs
+            echo $PIDSS > /dev/stune/foreground/cgroup.procs
+
+            PIDAIO=`ps -AT | grep android.io | awk '{print $3}'`
+            echo $PIDAIO > /dev/stune/foreground/tasks
+
+            PIDAA=`ps -AT | grep android.anim | awk '{print $3}'`
+            echo $PIDAA > /dev/cpuset/top-app/tasks
+
+            PIDAALF=`ps -AT | grep android.anim.lf | awk '{print $3}'`
+            echo $PIDAALF > /dev/cpuset/top-app/tasks
+
+            PIDAFG=`ps -AT | grep android.fg | awk '{print $3}'`
+            echo $PIDAFG > /dev/stune/foreground/tasks
+
+            PIDAUI=`ps -AT | grep android.ui | awk '{print $3}'`
+            echo $PIDAUI > /dev/stune/top-app/tasks
+
+            PIDAD=`ps -AT | grep android.display | awk '{print $3}'`
+            echo $PIDAD > /dev/cpuset/top-app/tasks
+            echo $PIDAD > /dev/stune/top-app/tasks
+
+            PIDRECLAIMD=`ps -AT | grep reclaimd | awk '{print $3}'`
+            echo $PIDRECLAIMD > /dev/stune/top-app/tasks
+
             # Turn off scheduler boost at the end
             echo 0 > /proc/sys/kernel/sched_boost
 
