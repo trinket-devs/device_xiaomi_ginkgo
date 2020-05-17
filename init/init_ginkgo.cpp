c9142bcc45010b82ddbde118892c11375cfd81ef
cyberwalkman <firefox883@gmail.com>
2020-05-17 20:27:20 +0500

    ginkgo: Set dalvik-heap configuration dynamically
     * We have devices with 3, 4 and 6(ginkgo only)GB RAM.
     * We can leave 4GB config in device.mk for those cases,
       when libinit does not work, for example in GSI builds.
    
    Change-Id: I5eef1e092e54444c73bd3f92bba7291a54321253

--- init/init_ginkgo.cpp
+++ init/init_ginkgo.cpp
@@ -32,6 +32,7 @@
 #include <fstream>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
+#include <sys/sysinfo.h>
 
 #include <android-base/properties.h>
 #include "property_service.h"
@@ -40,6 +41,46 @@
 using android::base::GetProperty;
 using android::init::property_set;
 using std::string;
+
+char const *heapstartsize;
+char const *heapgrowthlimit;
+char const *heapsize;
+char const *heapminfree;
+char const *heapmaxfree;
+char const *heaptargetutilization;
+
+void check_device()
+{
+    struct sysinfo sys;
+
+    sysinfo(&sys);
+
+    if (sys.totalram > 5072ull * 1024 * 1024) {
+        // from - phone-xhdpi-6144-dalvik-heap.mk
+        heapstartsize = "16m";
+        heapgrowthlimit = "256m";
+        heapsize = "512m";
+        heaptargetutilization = "0.5";
+        heapminfree = "8m";
+        heapmaxfree = "32m";
+    } else if (sys.totalram > 3072ull * 1024 * 1024) {
+        // from - phone-xhdpi-4096-dalvik-heap.mk
+        heapstartsize = "8m";
+        heapgrowthlimit = "192m";
+        heapsize = "512m";
+        heaptargetutilization = "0.6";
+        heapminfree = "8m";
+        heapmaxfree = "16m";
+    } else {
+        // from - phone-xhdpi-2048-dalvik-heap.mk
+        heapstartsize = "8m";
+        heapgrowthlimit = "192m";
+        heapsize = "512m";
+        heaptargetutilization = "0.75";
+        heapminfree = "512k";
+        heapmaxfree = "8m";
+    }
+}
 
 void property_override(string prop, string value)
 {
@@ -67,6 +108,15 @@ bool has_tianma_panel()
 
 void vendor_load_properties()
 {
+    check_device();
+
+    property_set("dalvik.vm.heapstartsize", heapstartsize);
+    property_set("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
+    property_set("dalvik.vm.heapsize", heapsize);
+    property_set("dalvik.vm.heaptargetutilization", heaptargetutilization);
+    property_set("dalvik.vm.heapminfree", heapminfree);
+    property_set("dalvik.vm.heapmaxfree", heapmaxfree);
+
     string device, model, desc;
     string fp = "google/coral/coral:10/QQ2A.200501.001.B2/6352890:user/release-keys";
 
