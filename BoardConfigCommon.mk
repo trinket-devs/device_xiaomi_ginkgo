9b24696d0f372d9a48a88d538387b22246e03abb
Jaihind Yadav <jaihindyadav@codeaurora.org>
2020-05-20 09:53:29 +0530

    trinket-common: Update sepolicy.mk path to point to vendor sepolicy.mk
    Due to split of sepolicy , Now the vendor Boardconfig.mk should
    call to venodor sepolicy.mk file.
    
    Change-Id: Ie6a930b2310c372b5e570b68372c896ea77ddbd7

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -162,7 +162,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD := true
 
 # Sepolicy
-include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/public
 BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
