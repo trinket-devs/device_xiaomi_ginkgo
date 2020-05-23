fd4fc84408bb1128aa258bac696ca19badca776c
Mrinal Ghosh <mg712702@gmail.com>
2020-05-23 15:56:30 +0530

    ginkgo: Remove prebuilt ANXCamera
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- lineage_ginkgo.mk
+++ lineage_ginkgo.mk
@@ -14,7 +14,6 @@ $(call inherit-product, device/xiaomi/ginkgo/device.mk)
 
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-$(call inherit-product, vendor/ANXCamera/config.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_ginkgo
