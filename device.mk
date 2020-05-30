51565dd23fe1c6f3d8041d5358e380388691fb36
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-05-31 00:17:24 +0530

    ginkgo: configs: Update media profiles from CAF
     * From LA.UM.8.11.r1-03500-NICOBAR.0 media HAL configs
     * Enables 4k30 recording support in ANXCamera
     * Added encoder profiles for aux cameras manually
     * Removed redundant media_profiles_vendor.xml
    
    Change-Id: Ic5e76ea4209d29fb44cee775fbded1c4305819c5

--- device.mk
+++ device.mk
@@ -244,7 +244,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
     $(LOCAL_PATH)/configs/media/media_codecs_vendor_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor_audio.xml \
     $(LOCAL_PATH)/configs/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
-    $(LOCAL_PATH)/configs/media/media_profiles_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml \
     $(LOCAL_PATH)/configs/media/system_properties.xml:$(TARGET_COPY_OUT_VENDOR)/etc/system_properties.xml
 
 PRODUCT_COPY_FILES += \
