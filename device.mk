ccf273b8f52b0898150c19d744c8ca77844840fb
Adithya R <radithya2002@gmail.com>
2020-06-06 13:02:43 +0530

    ginkgo: Switch to prebuilt audio stack
     * From willow V11.0.9.0.PCXEUXM
     * Required for audio_hw_socket support for MiSound
    
    06-06 02:17:57.226 I/EffectMiSound(729): MiSoundsetParameters_request: cal_devid=4;cal_apptype=69936;cal_caltype=0;cal_samplerate=48000;cal_topoid=0x1000a101;cal_moduleid=0x1000a101;cal_paramid=0x1000a105;cal_persist=0;cal_data=AAAAAA;
    06-06 02:17:57.226 E/EffectMiSound(729): clientConnect: Error connect server socket /dev/socket/audio_hw_socket Connection refused
    06-06 02:17:57.226 E/EffectMiSound(729): clientRequest: sockfd not connect -1
    06-06 02:17:57.226 I/EffectMiSound(729): Thread: setParam No.2
    
    Change-Id: Icc36e4e2d5485724ef8a023519a5649a78f809bf

--- device.mk
+++ device.mk
@@ -25,9 +25,9 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio.effect@5.0-impl \
+    android.hardware.audio.effect@4.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio@5.0-impl \
+    android.hardware.audio@4.0-impl \
     android.hardware.soundtrigger@2.2-impl \
     audio.a2dp.default \
     audio.primary.trinket \
@@ -36,17 +36,6 @@ PRODUCT_PACKAGES += \
     liba2dpoffload \
     libaudio-resampler \
     libaudioroute \
-    libbatterylistener \
-    libcomprcapture \
-    libexthwplugin \
-    libhdmiedid \
-    libqcompostprocbundle \
-    libqcomvisualizer \
-    libqcomvoiceprocessing \
-    libsndmonitor \
-    libspkrprot \
-    libtinycompress \
-    libtinycompress.vendor \
     libvolumelistener \
     tinymix
 
--- extract-files.sh
+++ extract-files.sh
@@ -64,6 +64,9 @@ find $BLOB_ROOT -type f | xargs sed -i "s/android.hidl.base@1.0.so/libhidlbase.s
 
 sed -i 's/libui.so/libuq.so/g' "${BLOB_ROOT}/vendor/lib/hw/camera.trinket.so"
 
+patchelf --add-needed "libprocessgroup.so" "${BLOB_ROOT}/vendor/lib/hw/audio.primary.trinket.so"
+patchelf --add-needed "libprocessgroup.so" "${BLOB_ROOT}/vendor/lib64/hw/audio.primary.trinket.so"
+
 patchelf --remove-needed "vendor.xiaomi.hardware.mtdservice@1.0.so" "${BLOB_ROOT}/vendor/bin/mlipayd@1.1"
 patchelf --remove-needed "vendor.xiaomi.hardware.mtdservice@1.0.so" "${BLOB_ROOT}/vendor/lib64/libmlipay.so"
 patchelf --remove-needed "vendor.xiaomi.hardware.mtdservice@1.0.so" "${BLOB_ROOT}/vendor/lib64/libmlipay@1.1.so"
