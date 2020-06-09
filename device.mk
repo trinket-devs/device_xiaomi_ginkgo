f519fe0e38ebf18c73768c08218056ed7c585e32
Adithya R <radithya2002@gmail.com>
2020-06-09 15:42:49 +0530

    ginkgo: Switch to prebuilt QTI mapper and libgrallocutils
     * From laurel_sprout stock 10.0
    
     * Contains xiaomeme's RAW10 buffer hack to fix HDR+ shots
       with google camera on trinket devices
    
    Change-Id: I2269c3a40194c2d9431c2528f86fd006dca59d95

--- device.mk
+++ device.mk
@@ -107,7 +107,6 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.3-service \
-    android.hardware.graphics.mapper@2.0-impl-qti-display \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
     gralloc.trinket \
