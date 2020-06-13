1fa6508a1bab1ab4910f90d253b8e980fb2f9afe
Adithya R <radithya2002@gmail.com>
2020-06-13 19:47:31 +0530

    ginkgo: Drop tianma panel KCAL hack
     * On ginkgo we have a weird bug where changing KCAL
       values takes effect only on toggling night light,
       so there's no way of making sure RGB is really set
       to 230 on boot even though the nodes will report 230
    
     * Let's implement this in the SDE driver in our kernel
       so we can make sure RGB is set to 230 during SDE
       initialization itself.
    
    Change-Id: Iaae40a5c2d292f32774409623215deaf58d626dd

--- init/init_ginkgo.cpp
+++ init/init_ginkgo.cpp
@@ -28,8 +28,6 @@
 */
 
 #include <cstdlib>
-#include <cstdio>
-#include <fstream>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
 #include <sys/sysinfo.h>
@@ -50,20 +48,6 @@ void property_override(string prop, string value)
         __system_property_update(pi, value.c_str(), value.size());
     else
         __system_property_add(prop.c_str(), prop.size(), value.c_str(), value.size());
-}
-
-bool has_tianma_panel()
-{
-    std::ifstream cmdline("/proc/cmdline");
-    string line;
-    bool ret = false;
-
-    std::getline(cmdline, line);
-    if (line.find("tianma") != string::npos)
-        ret = true;
-
-    cmdline.close();
-    return ret;
 }
 
 void vendor_load_properties()
@@ -136,25 +120,4 @@ void vendor_load_properties()
     property_set("dalvik.vm.heaptargetutilization", heaptargetutilization);
     property_set("dalvik.vm.heapminfree", heapminfree);
     property_set("dalvik.vm.heapmaxfree", heapmaxfree);
-
-    // Hax for Tianma panels burn-in issue
-    // https://forum.xda-developers.com/redmi-note-8/how-to/huge-issue-custom-roms-spoiling-display-t4075133
-    if (has_tianma_panel()) {
-        FILE *red = fopen("/sys/module/msm_drm/parameters/kcal_red", "wb");
-        FILE *green = fopen("/sys/module/msm_drm/parameters/kcal_green", "wb");
-        FILE *blue = fopen("/sys/module/msm_drm/parameters/kcal_blue", "wb");
-
-        // Incase the kernel doesn't have KCAL support
-        if (red != NULL && green != NULL && blue != NULL) {
-            // 90% is the desired rgb value to prevent burn-in in Tianma panels
-            // 90% of default value 256 = 230.4 ~= 230
-            fprintf(red, "230\n");
-            fprintf(green, "230\n");
-            fprintf(blue, "230\n");
-
-            fclose(red);
-            fclose(green);
-            fclose(blue);
-        }
-    }
 }
