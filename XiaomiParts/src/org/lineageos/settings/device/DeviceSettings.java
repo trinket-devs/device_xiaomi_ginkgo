abc1726dbb9fa4029e3012f20076ed0b03f81799
ronaxdevil <pratabidya.007@gmail.com>
2020-06-18 09:40:23 +0000

    ginkgo: XiaomiParts: USB Fastcharge improvement
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- XiaomiParts/src/org/lineageos/settings/device/DeviceSettings.java
+++ XiaomiParts/src/org/lineageos/settings/device/DeviceSettings.java
@@ -112,8 +112,9 @@ public class DeviceSettings extends PreferenceFragment implements
 
         if (FileUtils.fileWritable(USB_FASTCHARGE_PATH)) {
             mFastcharge = (SecureSettingSwitchPreference) findPreference(PREF_USB_FASTCHARGE);
-            mFastcharge.setChecked(FileUtils.getFileValueAsBoolean(USB_FASTCHARGE_PATH, true));
-            mFastcharge.setOnPreferenceChangeListener(this);
+            mFastcharge.setEnabled(Fastcharge.isSupported());
+            mFastcharge.setChecked(Fastcharge.isCurrentlyEnabled(this.getContext()));
+            mFastcharge.setOnPreferenceChangeListener(new Fastcharge(getContext()));
         } else { getPreferenceScreen().removePreference(findPreference(CATEGORY_FASTCHARGE)); }
 
         if (FileUtils.fileWritable(VIBRATION_STRENGTH_PATH)) {
@@ -152,10 +153,6 @@ public class DeviceSettings extends PreferenceFragment implements
                     getContext().startService(new Intent(getContext(), DiracService.class));
                     DiracService.sDiracUtils.setLevel(String.valueOf(value));
                 }
-                break;
-
-            case PREF_USB_FASTCHARGE:
-                FileUtils.setValue(USB_FASTCHARGE_PATH, (boolean) value);
                 break;
 
             case PREF_VIBRATION_STRENGTH:
