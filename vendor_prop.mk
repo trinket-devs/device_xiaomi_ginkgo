9eee27d899d1000a4576a40482d8588071ed504d
Mrinal Ghosh <mg712702@gmail.com>
2020-06-24 16:32:59 +0530

    ginkgo: Enable bluetooth A2DP offload
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -91,14 +91,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.bluetooth.a2dp_offload.cap=sbc-aptx-aptxtws-aptxhd-aac-ldac-aptxadaptive \
-    persist.bluetooth.a2dp_offload.disabled=true \
-    persist.bluetooth.bluetooth_audio_hal.disabled=true \
+    persist.bluetooth.a2dp_offload.cap=sbc-aac-aptx-aptxhd-ldac \
+    persist.bluetooth.a2dp_offload.disabled=false \
+    persist.vendor.bt.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac \
     persist.vendor.bt.aac_frm_ctl.enabled=true \
-    persist.vendor.bt.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldac-aptxadaptive \
-    persist.vendor.qcom.bluetooth.enable.splita2dp=false \
-    ro.bluetooth.a2dp_offload.supported=false \
-    vendor.bluetooth.soc=cherokee \
+    persist.vendor.qcom.bluetooth.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldac \
+    persist.vendor.qcom.bluetooth.enable.splita2dp=true \
+    persist.vendor.qcom.bluetooth.twsp_state.enabled=false \
+    ro.bluetooth.a2dp_offload.supported=true \
     vendor.qcom.bluetooth.soc=cherokee
 
 # CNE and DPM
