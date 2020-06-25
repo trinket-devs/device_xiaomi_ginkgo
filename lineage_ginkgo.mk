dc99c9909a10cffbf28d3d1838c4fa27b0a94eb0
Mrinal Ghosh <mg712702@gmail.com>
2020-06-25 08:33:44 +0530

    ginkgo: Switch to miuicamera

--- lineage_ginkgo.mk
+++ lineage_ginkgo.mk
@@ -15,7 +15,7 @@ $(call inherit-product, device/xiaomi/ginkgo/device.mk)
 
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-$(call inherit-product, vendor/ANXCamera/config.mk)
+$(call inherit-product, vendor/miuicamera/common/common-vendor.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_ginkgo
