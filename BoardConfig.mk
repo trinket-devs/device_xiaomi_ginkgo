971187c0fb78c19712f5d6844f7825583ace2cc4
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-07-01 13:53:06 +0530

    ginkgo: Import device-specific sepolicies
     * Decommonized from trinket-common

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,6 +48,10 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 # Releasetools
 TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 
+# Sepolicy
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+
 # Inherit from the proprietary version
 -include vendor/xiaomi/ginkgo/BoardConfigVendor.mk
 -include vendor/miuicamera/common/BoardConfigVendor.mk
