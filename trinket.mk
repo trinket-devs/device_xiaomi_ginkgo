54a6aaae342b3dbde15940b576bbaa12697fb706
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-07-10 21:55:59 +0530

    trinket-common: Split props into system and vendor

--- trinket.mk
+++ trinket.mk
@@ -8,9 +8,6 @@
 # Vendor blobs
 $(call inherit-product, vendor/xiaomi/trinket-common/trinket-common-vendor.mk)
 
-# Vendor properties
--include $(LOCAL_PATH)/vendor_prop.mk
-
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
