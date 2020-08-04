1e62656d2f301a0dfad15d0a6ccbf4e78be8cca8
Mrinal Ghosh <mg712702@gmail.com>
2020-08-04 15:41:06 +0530

    ginkgo: init: Update build fingerprint from Coral's August release
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- init/init_ginkgo.cpp
+++ init/init_ginkgo.cpp
@@ -52,7 +52,7 @@ void property_override(string prop, string value)
 void vendor_load_properties()
 {
     string device, model, desc;
-    string fp = "google/coral/coral:10/QQ3A.200705.002/6506677:user/release-keys";
+    string fp = "google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keys";
 
     string region = GetProperty("ro.boot.hwc", "");
     string hwversion = GetProperty("ro.boot.hwversion", "");
