11c639dd11d388c281be342051d10e37f5b8c537
Kavya Nunna <knunna@codeaurora.org>
2020-08-20 19:11:12 +0530

    ginkgo: vibrator: Enable LED based vibrator
    Enable LED based vibrator for trinket and bengal
    targets.
    
    Change-Id: I7e434c2df3e57dd706c81cc7b719cc972fa77fbc

--- device.mk
+++ device.mk
@@ -409,8 +409,7 @@ PRODUCT_COPY_FILES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
+    vendor.qti.hardware.vibrator.service
 
 # Wifi
 PRODUCT_PACKAGES += \
