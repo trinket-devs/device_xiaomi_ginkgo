3e98ecebffd6af9c4ce7ff0df7f425865148db7b
Joey Huab <joey@evolution-x.org>
2020-09-09 19:49:10 +0000

    ginkgo: Update to Coral's Sept 2020 fingerprint
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- init/init_ginkgo.cpp
+++ init/init_ginkgo.cpp
@@ -53,7 +53,7 @@ void property_override(string prop, string value)
 void vendor_load_properties()
 {
     string device, model, desc;
-    string fp = "google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keys";
+    string fp = "google/coral/coral:11/RP1A.200720.009/6720564:user/release-keys";
 
     string region = GetProperty("ro.boot.hwc", "");
     string hwversion = GetProperty("ro.boot.hwversion", "");
