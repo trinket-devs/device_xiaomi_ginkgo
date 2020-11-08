814b7f1a8072280f1fe5cead32bde9bd6a170af8
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-11-08 09:07:20 +0530

    trinket-common: Disable wifi aware and RTT
     * as per ginkgo stock
     * fixes logspam:
    11-08 09:05:18.838   691   691 E WifiHAL : error_handler_nan: Error code:-95 (Operation not supported on transport endpoint)
    11-08 09:05:18.838   691   691 E WifiHAL : nan_disable_request: requestEvent Error:-3
    11-08 09:05:18.838   691   691 E WifiHAL : error_handler_nan: Error code:-22 (Invalid argument)
    11-08 09:05:18.838   691   691 E WifiHAL : nan_data_interface_delete: requestEvent Error:-5
    11-08 09:05:18.838   691   691 E WifiHAL : error_handler_nan: Error code:-22 (Invalid argument)
    11-08 09:05:18.838   691   691 E WifiHAL : nan_data_interface_delete: requestEvent Error:-5
    11-08 09:05:18.839  1236  3352 E WifiAwareNativeApi: disable: null interface
    
    Change-Id: I6c54404cbb2d59a60ef4ad2466d892ec3a0f266c

--- trinket.mk
+++ trinket.mk
@@ -269,10 +269,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml \
     frameworks/native/data/etc/android.hardware.vulkan.level-1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level-1.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version-1_1.xml \
-    frameworks/native/data/etc/android.hardware.wifi.aware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.aware.xml \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml \
-    frameworks/native/data/etc/android.hardware.wifi.rtt.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.rtt.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml \
