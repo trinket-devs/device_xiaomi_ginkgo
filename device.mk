c4e2a216f32ea6fa21dc0bd918d2c9fc6e42426a
Alexander Koskovich <zvnexus@outlook.com>
2020-11-18 15:10:31 -0700

    ginkgo: Use the new AIDL QTI thermal HAL
    Change-Id: Ib9d983ad3eea58d840d2c5f8b42d5c6845b69906
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- device.mk
+++ device.mk
@@ -398,6 +398,9 @@ PRODUCT_BOOT_JARS += \
     telephony-ext
 
 # Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0-service.qti
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf \
     $(LOCAL_PATH)/configs/thermal/thermal-engine-camera.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-camera.conf \
