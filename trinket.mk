2baf896c98d3cba8eaf67b930dbcf436e5d669c3
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-11-28 16:41:14 +0530

    trinket-common: Nuke Camera2

--- trinket.mk
+++ trinket.mk
@@ -70,7 +70,6 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    Camera2 \
     android.frameworks.displayservice@1.0 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
