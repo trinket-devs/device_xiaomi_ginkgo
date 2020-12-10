e7064ad879e3febc3dfad75faef0048c3c21cf39
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-12-11 01:16:16 +0530

    ginkgo: Update redfin fingerprint to Dec 2020

--- arrow_ginkgo.mk
+++ arrow_ginkgo.mk
@@ -37,8 +37,8 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 DEVICE_MAINTAINER := Adithya R
 
 # Fingerprint
-BUILD_FINGERPRINT := google/redfin/redfin:11/RD1A.201105.003/6886239:user/release-keys
-BUILD_DESCRIPTION := redfin-user 11 RD1A.201105.003 6886239 release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ1A.201205.010/6953398:user/release-keys
+BUILD_DESCRIPTION := redfin-user 11 RQ1A.201205.010 6953398 release-keys
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
