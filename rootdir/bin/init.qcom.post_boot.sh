f1a1d02fd4ea305a04b62b4eda7f03a95cd49ce2
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-12-16 16:30:56 +0530

    trinket-common: QCOM post_boot tuning for trinket - part 2
     * kang schedutil ratelimit from sunfish
     * bump up/down migration threshold to reduce load on big cluster
     * enable foreground schedtune prefer_idle to improve UI performance
     * enable WALT predicted load boosting
     * disable sched_walt_rotate_big_tasks (credits to celtare21)

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -3083,8 +3083,8 @@ case "$target" in
             echo 4 > /sys/devices/system/cpu/cpu4/core_ctl/task_thres
 
             # Setting b.L scheduler parameters
-            echo 67 > /proc/sys/kernel/sched_downmigrate
-            echo 77 > /proc/sys/kernel/sched_upmigrate
+            echo 85 > /proc/sys/kernel/sched_downmigrate
+            echo 85 > /proc/sys/kernel/sched_upmigrate
             echo 85 > /proc/sys/kernel/sched_group_downmigrate
             echo 100 > /proc/sys/kernel/sched_group_upmigrate
 
@@ -3096,24 +3096,27 @@ case "$target" in
             echo 0-3 > /dev/cpuset/restricted/cpus
 
             # schedtune settings
+            echo 1 > /dev/stune/foreground/schedtune.prefer_idle
             echo 1 > /dev/stune/top-app/schedtune.boost
             echo 1 > /dev/stune/top-app/schedtune.prefer_idle
 
             # configure governor settings for little cluster
             echo "schedutil" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-            echo 1000 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/up_rate_limit_us
-            echo 1000 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/down_rate_limit_us
+            echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/pl
+            echo 500 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/up_rate_limit_us
+            echo 20000 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/down_rate_limit_us
             echo 1420800 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/hispeed_freq
             echo 864000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
 
             # configure governor settings for big cluster
             echo "schedutil" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
-            echo 1000 > /sys/devices/system/cpu/cpu4/cpufreq/schedutil/up_rate_limit_us
-            echo 1000 > /sys/devices/system/cpu/cpu4/cpufreq/schedutil/down_rate_limit_us
+            echo 1 > /sys/devices/system/cpu/cpu4/cpufreq/schedutil/pl
+            echo 500 > /sys/devices/system/cpu/cpu4/cpufreq/schedutil/up_rate_limit_us
+            echo 20000 > /sys/devices/system/cpu/cpu4/cpufreq/schedutil/down_rate_limit_us
             echo 1401600 > /sys/devices/system/cpu/cpu4/cpufreq/schedutil/hispeed_freq
             echo 652800 > /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq
 
-            echo 1 > /proc/sys/kernel/sched_walt_rotate_big_tasks
+            echo 0 > /proc/sys/kernel/sched_walt_rotate_big_tasks
 
             # sched_load_boost as -6 is equivalent to target load as 85. It is per cpu tunable.
             echo -6 >  /sys/devices/system/cpu/cpu0/sched_load_boost
