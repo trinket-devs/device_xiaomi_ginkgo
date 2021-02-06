02eb4e2712cf199ca31eba35b5a90ed24acab826
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-02-06 21:50:23 +0530

    ginkgo: Drop pre-uplink-features netmgrd flag
     * not needed with R blobs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -179,7 +179,6 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 
 # RIL
 ENABLE_VENDOR_RIL_SERVICE := true
-TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD := true
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
