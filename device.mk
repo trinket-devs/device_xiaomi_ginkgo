0583f7832fc67a72b1117bef92e2a16ed0696f31
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-02-07 11:56:23 +0530

    ginkgo: Switch to sensors@2.0 MultiHAL service
     * for R sensor stack

--- device.mk
+++ device.mk
@@ -356,8 +356,7 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     libsensorndkbridge \
-    android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service \
+    android.hardware.sensors@2.0-service.multihal
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
