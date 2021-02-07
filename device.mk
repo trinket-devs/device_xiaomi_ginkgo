1613c4de87f1d0d9987a11f70b59008f37cb952b
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-02-07 12:24:53 +0530

    ginkgo: Switch back to MiuiCamera
     * at last we meet again, old friend.

--- device.mk
+++ device.mk
@@ -87,8 +87,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml
 
 # Camera
-PRODUCT_PACKAGES += \
-    GCamGOPrebuilt
+$(call inherit-product, vendor/miuicamera/config.mk)
 
 PRODUCT_PACKAGES += \
     libmedia_jni_shim
