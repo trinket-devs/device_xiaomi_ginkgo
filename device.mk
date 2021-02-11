5c94b62ae417e47ac35bff1d53f784c8d14063d1
Mrinal Ghosh <mg712702@gmail.com>
2021-02-12 05:00:53 +0530

    ginkgo: Drop XiaomiDoze
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- device.mk
+++ device.mk
@@ -151,10 +151,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.level-1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level-1.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version-1_1.xml
 
-# Doze
-PRODUCT_PACKAGES += \
-    XiaomiDoze
-
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3-service.clearkey
