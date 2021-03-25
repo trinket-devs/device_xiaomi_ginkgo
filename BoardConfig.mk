8c4fd6bedfc4729b35217406b65cb7253db7e7b0
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-03-25 21:12:36 +0530

    ginkgo: Disable useless LMKD stats logging
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -139,9 +139,6 @@ TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CLANG_VERSION := sdllvm
 TARGET_KERNEL_ADDITIONAL_FLAGS := LD=ld.lld AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip
 
-# LMKD
-TARGET_LMKD_STATS_LOG := true
-
 # NFC
 TARGET_USES_NQ_NFC := true
 
