34bf7f44b0566b93000195f0c0f282b9b31459c0
Mrinal Ghosh <mg712702@gmail.com>
2021-04-10 22:38:51 +0530

    ginkgo: Remove more prebuilt apps
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt AppDirectedSMSService Camera2 ConnMO DCMO MyVerizonServices OBDM_Permissions OemDmTrigger Snap SprintDM SprintHM USCCDM VZWAPNLib VzwOmaTrigger obdm_stub
+LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt AndroidAutoStubPrebuilt AppDirectedSMSService Camera2 ConnMO DCMO FilesPrebuilt MaestroPrebuilt MyVerizonServices OBDM_Permissions OemDmTrigger obdm_stub SafetyHubPrebuilt Snap SoundAmplifierPrebuilt SprintDM SprintHM Tycho USCCDM VZWAPNLib VzwOmaTrigger WallpapersBReel2020
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
