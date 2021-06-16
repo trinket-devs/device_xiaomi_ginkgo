b802c6d1840abd997d94b0968bc281cc096b345b
Mrinal Ghosh <mg712702@gmail.com>
2021-06-16 12:41:48 +0530

    ginkgo: ginkgo: Update fp/desc from redfin's june release
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- lineage_ginkgo.mk
+++ lineage_ginkgo.mk
@@ -35,8 +35,8 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Fingerprint
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys
-BUILD_DESCRIPTION := redfin-user 11 RQ2A.210505.003 7255357 release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210605.005/7349499:user/release-keys
+BUILD_DESCRIPTION := redfin-user 11 RQ3A.210605.005 7349499 release-keys
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
